96fc93accaf155fa241e889c7d3d37b22a77ea00
Web4 <167559384+Web4application@users.noreply.github.com>
2025-07-25 00:04:40 +0200

    Delete bash.sh

--- bash.sh
+++
@@ -1,9 +0,0 @@
-npm install vue-router pinia ethers
-# Optional (recommended):
-npm install vue-toastification
-npm create vite@latest roda-web4 -- --template vue
-cd roda-web4
-npm install
-
-az login
-az webapp up --name roda-ai-app --runtime "NODE|18-lts" --sku F1
