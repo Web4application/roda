9c0e46b132e79c6448bdbe89026cf726ae4b7196
Web4 <167559384+Web4application@users.noreply.github.com>
2025-07-24 23:33:50 +0200

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -4,3 +4,6 @@ npm install vue-toastification
 npm create vite@latest roda-web4 -- --template vue
 cd roda-web4
 npm install
+
+az login
+az webapp up --name roda-ai-app --runtime "NODE|18-lts" --sku F1
